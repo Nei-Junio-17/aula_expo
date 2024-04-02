80fab4b0b823ea8a010216986e7d00bc5f11e6c0
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-04-01 22:01:38 -0300

    exemplo 4
    Corrigir erro na cor da fonte do botão

--- src/atividade 4/index.js
+++ src/atividade 4/index.js
@@ -1,14 +1,44 @@
-import { View, Text, TextInput } from "react-native";
+import { useState } from 'react';
+import { View, Text, TextInput, Pressable } from "react-native";
+
 import styles from "../atividade 4/styles";
 
-export default function Exemplo4(){
-    return(
+export default function Exemplo4() {
+
+    const [txtDigitado, settxtDigitado] = useState('Texto Digitado');
+    const [txt2, setTxt2] = useState('');
+    const [txtTela, setTxtTela] = useState('');
+
+    function atualizaTextoHandle(txt) {
+        settxtDigitado(txt);
+    }
+
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 4</Text>
 
-            <Text style={styles.texto}>Texto</Text>
-            
-            <TextInput style={styles.input} />
+            <Text style={styles.texto}>{txtDigitado}</Text>
+
+            <TextInput
+                style={styles.input}
+                onChangeText={(valor) => atualizaTextoHandle(valor)}
+            />
+
+            <View style={styles.linha} />
+
+            <Text style={styles.texto}>{txtTela}</Text>
+
+            <TextInput
+                style={styles.input}
+                onChangeText={(vlr) => setTxt2(vlr)}
+                value={txt2}
+            />
+
+            <Pressable style={({ pressed }) => pressed ? [styles.botao, styles.botaoPress] : styles.botao}>
+                <Text sytle={styles.txtBotao}>Exibir texto</Text>
+            </Pressable>
         </View>
     );
 }
+
+// Handle:  coloca no nome da função para indicar que ela foi criada no proprio arquivo, e não chamada de um outro arquivo
